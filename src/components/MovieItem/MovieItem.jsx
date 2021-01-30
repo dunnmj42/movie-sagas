775b73958f738f5c2f57ea94b5667ada6b37fddc
Michael Dunn <dunnmj42@gmail.com>
2021-01-29 18:02:28 -0600

    Grid layout initial config

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,26 +1,29 @@
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import { makeStyles } from '@material-ui/core/styles';
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 300,
-  }
+    maxWidth: 345,
+    minHeight: 415,
+  },
 }));
 
-function MovieItem({movie}) {
-
+function MovieItem({ movie }) {
   const classes = useStyles();
 
   return (
-    <Card className={classes.root}>
-      <CardHeader title={movie.title}/>
-      <CardContent>
-      <img src={movie.poster} alt={movie.title} />
-      </CardContent>
-    </Card>
+    <Grid item>
+      <Card className={classes.root}>
+        <CardHeader title={movie.title} />
+        <CardContent>
+          <img src={movie.poster} alt={movie.title} />
+        </CardContent>
+      </Card>
+    </Grid>
   );
-};
+}
 
 export default MovieItem;
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,11 +1,18 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import './MovieList.css'
 import MovieItem from '../MovieItem/MovieItem';
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+  }));
 
 function MovieList() {
 
+    const classes = useStyles();
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
 
@@ -16,13 +23,15 @@ function MovieList() {
     return (
         <main>
             <h1>MovieList</h1>
-            <section className="movies">
+            <div className={classes.root}>
+            <Grid container spacing={3} justify="center" alignItems="center">
                 {movies.map(movie => {
                     return (
                         <MovieItem key={movie.id} movie={movie} />
                     );
                 })}
-            </section>
+            </Grid>
+            </div>
         </main>
 
     );
