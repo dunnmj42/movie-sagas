e4e55e81262a758d1413ef4e05a72bac9c33deec
Michael Dunn <dunnmj42@gmail.com>
2021-01-31 16:02:11 -0600

    app mui style migration

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,22 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
-import './App.css';
 import MovieList from '../MovieList/MovieList'
 import NavBar from '../NavBar/NavBar';
 import Details from '../Details/Details';
 import AddMovie from '../AddMovie/AddMovie';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: 'center',
+  },
+}));
 
 function App() {
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
+    <div className={classes.root}>
       <NavBar />
       <Router>        
         <Route path="/" exact>
