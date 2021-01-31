3e902ebbfd75e11f10c331b31480ef6af7f71163
Michael Dunn <dunnmj42@gmail.com>
2021-01-30 20:01:06 -0600

    navbar link to add movie

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,33 @@
 import Paper from '@material-ui/core/Paper';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > * + *': {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}));
 
 function NavBar() {
+
+  const classes = useStyles();
+  const preventDefault = (event) => event.preventDefault();
 
   return (
     <div>
       <Paper>
+        <br/>
         <h1>The Movies Saga!</h1>
+        <br/>
+        <Typography className={classes.root}>
+          <Link href="/#/addmovie" onClick={preventDefault}>
+            Add Movie  
+          </Link>
+        </Typography>
+        <br/>
       </Paper>
     </div>
   );
